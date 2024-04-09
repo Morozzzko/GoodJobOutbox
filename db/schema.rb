bb7511f8033989563fc20ab5e547cd22c4cc3e97
Igor S. Morozov <igor@hoodies.team>
2024-04-09 18:36:07 +0200

    Add unique index so that the transaction WILL have to roll back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_09_162303) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_09_163502) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -97,6 +97,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_09_162303) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["text"], name: "index_good_quotes_on_text", unique: true
   end
 
 end
